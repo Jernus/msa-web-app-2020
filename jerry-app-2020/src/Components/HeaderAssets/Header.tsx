4228b6af1da2f7c2954fe496e5d0aa9dce0acbec
Jernus <jerry7626@yahoo.com.tw>
2020-07-25 18:30:27 +1200

    Change from feature branch

--- jerry-app-2020/src/Components/HeaderAssets/Header.tsx
+++ jerry-app-2020/src/Components/HeaderAssets/Header.tsx
@@ -6,6 +6,7 @@ function Header()
     return (
         <div className="header">
             <div>
+                <h1>This is develop branch</h1>
                 <h1><span id="head">Search for Most Popular Articles from  </span><span id="nyt">The New York Times</span></h1>
                 <div>
                     <h3 id="api">API:  <a href="https://developer.nytimes.com/docs/most-popular-product/1/overview" id="api-link">https://developer.nytimes.com/docs/most-popular-product/1/overview</a></h3>
